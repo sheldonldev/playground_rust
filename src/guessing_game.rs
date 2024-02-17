6c760d2b29595d53d594ba93bd3e3971a6b1ba6a
sheldonldev <xiadanli0320@gmail.com>
2024-02-17 21:58:03 +0800

    guessing game optimize how to quit

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -4,30 +4,40 @@ use std::io;
 
 pub fn guess() {
     // welcome words
-    println!("Game: guessing numbers!");
+    println!("Game: guessing numbers! (type 'quit' to quit)");
 
     // loop the game
     loop {
         // generate random secret number
         let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-        // get user input
-        let mut guess: String = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line!");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        // loop the guess till correct or quit
+        loop {
+            // get user input
+            let mut guess: String = String::new();
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line!");
 
-        // comparison arms
-        match guess.cmp(&secret_number) {
-            Ordering::Greater => println!("Too large."),
-            Ordering::Less => println!("Too small."),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+            // if 'quit' then quit
+            if guess.trim() == "quit" {
+                println!("Quit the game.");
+                return;
+            }
+
+            // parse to number or continue input
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            // comparison arms
+            match guess.cmp(&secret_number) {
+                Ordering::Greater => println!("Too large."),
+                Ordering::Less => println!("Too small."),
+                Ordering::Equal => {
+                    println!("You win! One more time. (type 'quit' to quit)");
+                    break;
+                }
             }
         }
     }
